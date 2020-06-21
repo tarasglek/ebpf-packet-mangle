a2b76460680ab7a04c5c2f8f47ee80fb6615ed36
Taras Glek <taras@purestorage.com>
2020-06-20 21:50:44 -0700

    checkpoint

--- load.sh
+++ load.sh
@@ -1,11 +1,12 @@
+set -e -x
 DEVICE=wlp3s0
 clang -g -O2 -Wall -target bpf -c tc-example.c -o tc-example.o
 
 tc filter show dev $DEVICE ingress
 
-tc qdisc add dev $DEVICE clsact
+# tc qdisc add dev $DEVICE clsact
 tc filter add dev $DEVICE ingress bpf da obj tc-example.o sec ingress
 tc filter add dev $DEVICE egress bpf da obj tc-example.o sec egress
 tc filter show dev $DEVICE ingress
 
-bpftool prog show
+# bpftool prog show
